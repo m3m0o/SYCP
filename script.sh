90467e744eb2aec7f25469c2d32f5806004836c9
m3m0o <guilherme.violin@hotmail.com>
2023-05-07 10:48:05 -0300

    Passagem de argumentos

--- script.sh
+++ script.sh
@@ -1,24 +1,16 @@
 #!/bin/bash
 
-echo -e "Digite (r) para Routes e (i) para Interfaces de Rede:\n"
-
-read option
-
-if [ $option == "r" ]
+if [ "$1" == "r" ]
 then
     echo -e "\nRotas:\n"
     route -n
-elif [ $option == "i" ]
+elif [ "$1" == "i" ]
 then
-    echo -e "\nDigite uma interface de rede:\n"
+    echo -e "\nEndereço IP da interface $2:\n"
 
-    read interface
-
-    echo -e "\nEndereço IP da interface $interface:\n"
-
-    result=$(ifconfig $interface | grep "inet" | head -n1)
+    result=$(ifconfig $2 | grep "inet" | head -n1)
 
     echo $result | cut -d' ' -f2
 else
-    echo -e "\nOpção inválida."
+    echo -e "\nUsage: ./script.sh r|i interface"
 fi
