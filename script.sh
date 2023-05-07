07c6aca1be3b5621ba6a5ce433ff42b62668e924
m3m0o <guilherme.violin@hotmail.com>
2023-05-07 10:33:14 -0300

    Condicionais

--- script.sh
+++ script.sh
@@ -1,6 +1,18 @@
 #!/bin/bash
 
-echo -e "Digite uma interface de rede:\n"
+echo -e "Digite (r) para Routes e (i) para Interfaces de Rede:\n"
+
+read option
+
+if [ "$option" == "r" ]
+then
+echo -e "\nRotas:\n"
+route -n
+fi
+
+if [ "$option" == "i" ]
+then
+echo -e "\nDigite uma interface de rede:\n"
 
 read interface
 
@@ -9,3 +21,4 @@ echo -e "\nEndereço IP da interface $interface:\n"
 result=$(ifconfig $interface | grep "inet" | head -n1)
 
 echo $result | cut -d' ' -f2
+fi
